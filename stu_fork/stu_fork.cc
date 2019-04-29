b8976d76cd3ac0e601ac560e66e68945a8b10538
xiaotaohe <taochao1997@qq.com>
2019-04-29 07:23:38 -0700

    循环创建多个子进程，能找到对应子进程

--- stu_fork/stu_fork.cc
+++ stu_fork/stu_fork.cc
@@ -19,12 +19,12 @@ int main()
   //答案不可以，第一个子进程第一次会逃过，第二次不会跳过，此时便会出现创建多个进程。
   //2.判断每个进程是那个进程
   if(i == 0)
-    printf("I am first fork!\n");
+    printf("I am first pid = %d fork!\n",getpid());
   if(i == 1)
-    printf("I am second fork!\n");
+    printf("I am second pid = %d fork!\n",getpid());
   if(i == 2)
-    printf("I am three fork!\n");
+    printf("I am three pid = %d fork!\n",getpid());
   if(i == 3)
-    printf("I am father fork!\n");
+    printf("I am father pid = %d fork!\n",getpid());
   return 0;
 }
