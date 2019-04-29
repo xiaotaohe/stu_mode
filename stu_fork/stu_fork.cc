77e2b75ba37929043ecca85f4eb660f77a699b91
xiaotaohe <taochao1997@qq.com>
2019-04-29 07:18:14 -0700

    循环创建多个子进程，能找到对应子进程

---
+++ stu_fork/stu_fork.cc
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/*
+ *本段目标：1.代码实现循环创建多个进程
+ *2.并判断是那个子进程
+ */
+int main()
+{
+  pid_t pid = 0;
+  int i = 0;
+  //1.循环创建3个子进程
+  for(i = 0;i<3;i++)
+  {
+    if((pid = fork()) == 0)
+      break;
+  }
+  //思考用continue可以吗？
+  //答案不可以，第一个子进程第一次会逃过，第二次不会跳过，此时便会出现创建多个进程。
+  //2.判断每个进程是那个进程
+  if(i == 0)
+    printf("I am first fork!\n");
+  if(i == 1)
+    printf("I am second fork!\n");
+  if(i == 2)
+    printf("I am three fork!\n");
+  if(i == 3)
+    printf("I am father fork!\n");
+  return 0;
+}
